5ed08e0f56727da1128c1874e7cb2f8d525f68e6
ariel <ariel@LAPTOP-MQ6B7FKQ.local>
2020-06-23 20:45:36 -0500

    Actualizacion Ventana Principal

--- Practica_5AlvaradoAndres_VazquezAriel/src/ec/edu/ups/vista/VentanaPrincipal.java
+++ Practica_5AlvaradoAndres_VazquezAriel/src/ec/edu/ups/vista/VentanaPrincipal.java
@@ -37,82 +37,126 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         menuBar = new javax.swing.JMenuBar();
         jMenuInicio = new javax.swing.JMenu();
-        jMenuItemRegistrarEntrada = new javax.swing.JMenuItem();
-        jMenuItemRegistrarSalida = new javax.swing.JMenuItem();
-        jMenuItemListar = new javax.swing.JMenuItem();
-        jMenuItemSalir = new javax.swing.JMenuItem();
+        jMenuItemIniciarSecion = new javax.swing.JMenuItem();
+        jMenuItemCerrar = new javax.swing.JMenuItem();
+        jMenuRegistros = new javax.swing.JMenu();
+        jMenuItemClientes = new javax.swing.JMenuItem();
+        jMenuItemVehiculos = new javax.swing.JMenuItem();
+        jMenuItemTickets = new javax.swing.JMenuItem();
         jMenuIdioma = new javax.swing.JMenu();
         jMenuItemEspañol = new javax.swing.JMenuItem();
         jMenuItemIngles = new javax.swing.JMenuItem();
+        jMenuAjustes = new javax.swing.JMenu();
+        jMenuItemCambiarColor = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(115, 189, 158));
+        jPanel1.setBackground(new java.awt.Color(48, 102, 109));
+
+        jLabel1.setFont(new java.awt.Font("Georgia", 1, 48)); // NOI18N
+        jLabel1.setText("Parqueadero UPS");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ec/edu/ups/imagenes/iconoPrincipal.png"))); // NOI18N
+        jLabel2.setText("jLabel2");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 950, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 404, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(206, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(205, 205, 205))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 624, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
-        jMenuInicio.setMnemonic('f');
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
-        jMenuItemRegistrarEntrada.setMnemonic('o');
-        jMenuInicio.add(jMenuItemRegistrarEntrada);
+        menuBar.setBackground(new java.awt.Color(255, 255, 255));
+        menuBar.setForeground(new java.awt.Color(0, 102, 102));
 
-        jMenuItemRegistrarSalida.setMnemonic('s');
-        jMenuInicio.add(jMenuItemRegistrarSalida);
+        jMenuInicio.setText("Inicio");
+        jMenuInicio.setFont(new java.awt.Font("Arial Narrow", 1, 20)); // NOI18N
 
-        jMenuItemListar.setMnemonic('a');
-        jMenuInicio.add(jMenuItemListar);
+        jMenuItemIniciarSecion.setText("Iniciar Sesion");
+        jMenuInicio.add(jMenuItemIniciarSecion);
 
-        jMenuItemSalir.setMnemonic('x');
-        jMenuItemSalir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItemSalirActionPerformed(evt);
-            }
-        });
-        jMenuInicio.add(jMenuItemSalir);
+        jMenuItemCerrar.setText("Cerrar");
+        jMenuInicio.add(jMenuItemCerrar);
 
         menuBar.add(jMenuInicio);
 
+        jMenuRegistros.setMnemonic('f');
+        jMenuRegistros.setText("Registros");
+        jMenuRegistros.setFocusTraversalPolicyProvider(true);
+        jMenuRegistros.setFont(new java.awt.Font("Arial Narrow", 1, 20)); // NOI18N
+
+        jMenuItemClientes.setMnemonic('o');
+        jMenuItemClientes.setText("Clientes");
+        jMenuRegistros.add(jMenuItemClientes);
+
+        jMenuItemVehiculos.setMnemonic('s');
+        jMenuItemVehiculos.setText("Vehiculos");
+        jMenuRegistros.add(jMenuItemVehiculos);
+
+        jMenuItemTickets.setMnemonic('a');
+        jMenuItemTickets.setText("Tickets");
+        jMenuRegistros.add(jMenuItemTickets);
+
+        menuBar.add(jMenuRegistros);
+
         jMenuIdioma.setMnemonic('e');
+        jMenuIdioma.setText("Idioma");
+        jMenuIdioma.setFont(new java.awt.Font("Arial Narrow", 1, 20)); // NOI18N
 
         jMenuItemEspañol.setMnemonic('t');
+        jMenuItemEspañol.setText("Español");
         jMenuIdioma.add(jMenuItemEspañol);
 
         jMenuItemIngles.setMnemonic('y');
+        jMenuItemIngles.setText("Ingles");
         jMenuIdioma.add(jMenuItemIngles);
 
         menuBar.add(jMenuIdioma);
 
-        setJMenuBar(menuBar);
+        jMenuAjustes.setText("Ajustes");
+        jMenuAjustes.setFont(new java.awt.Font("Arial Narrow", 1, 20)); // NOI18N
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jMenuItemCambiarColor.setText("Cambiar color del Fondo");
+        jMenuItemCambiarColor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemCambiarColorActionPerformed(evt);
+            }
+        });
+        jMenuAjustes.add(jMenuItemCambiarColor);
+
+        menuBar.add(jMenuAjustes);
+
+        setJMenuBar(menuBar);
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItemSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemSalirActionPerformed
-        System.exit(0);
-    }//GEN-LAST:event_jMenuItemSalirActionPerformed
+    private void jMenuItemCambiarColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemCambiarColorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItemCambiarColorActionPerformed
 
     /**
      * @param args the command line arguments
@@ -150,25 +194,35 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
     
     public void cambiarIdioma() {
-        jMenuInicio.setText(mensajes.getString("inicio"));
+        jMenuAjustes.setText(mensajes.getString("ajustes"));
         jMenuIdioma.setText(mensajes.getString("idioma"));
+        jMenuInicio.setText(mensajes.getString("inicio"));
+        jMenuItemCambiarColor.setText(mensajes.getString("cambiarColorDelFondo"));
+        jMenuItemCerrar.setText(mensajes.getString("cerrar"));
+        jMenuItemClientes.setText(mensajes.getString("clientes"));
         jMenuItemEspañol.setText(mensajes.getString("español"));
         jMenuItemIngles.setText(mensajes.getString("ingles"));
-        jMenuItemListar.setText(mensajes.getString("listar"));
-        jMenuItemRegistrarEntrada.setText(mensajes.getString("registroE"));
-        jMenuItemRegistrarSalida.setText(mensajes.getString("registroS"));
-        jMenuItemSalir.setText(mensajes.getString("salir"));
+        jMenuItemIniciarSecion.setText(mensajes.getString("iniciarSesion"));
+        jMenuItemTickets.setText(mensajes.getString("tickets"));
+        jMenuItemVehiculos.setText("vehiculos");
+        jMenuRegistros.setText(mensajes.getString("registros"));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenu jMenuAjustes;
     private javax.swing.JMenu jMenuIdioma;
     private javax.swing.JMenu jMenuInicio;
+    private javax.swing.JMenuItem jMenuItemCambiarColor;
+    private javax.swing.JMenuItem jMenuItemCerrar;
+    private javax.swing.JMenuItem jMenuItemClientes;
     private javax.swing.JMenuItem jMenuItemEspañol;
     private javax.swing.JMenuItem jMenuItemIngles;
-    private javax.swing.JMenuItem jMenuItemListar;
-    private javax.swing.JMenuItem jMenuItemRegistrarEntrada;
-    private javax.swing.JMenuItem jMenuItemRegistrarSalida;
-    private javax.swing.JMenuItem jMenuItemSalir;
+    private javax.swing.JMenuItem jMenuItemIniciarSecion;
+    private javax.swing.JMenuItem jMenuItemTickets;
+    private javax.swing.JMenuItem jMenuItemVehiculos;
+    private javax.swing.JMenu jMenuRegistros;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JMenuBar menuBar;
     // End of variables declaration//GEN-END:variables
